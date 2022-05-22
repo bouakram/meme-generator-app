54d338f78902117a0118b9f5ce5999a22085449c
Akrem Boughazi <kikoua203@gmail.com>
2022-05-22 23:56:19 +0200

    adding comments

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,16 @@ import React, { useState, useEffect } from 'react';
 
 function Main() {
 
+    // memes state
     const [memes , setMemes ] = useState({
         TopText : '',
         BottomText : '',
         RandomImg : img1
     })
+    // memes images state
     const [allMemeImg , setallMemeImg] = useState({})
 
+    //geetting hte data from an api
     // https://api.imgflip.com/get_memes
     useEffect(()=>{
         
@@ -19,20 +22,30 @@ function Main() {
         .then(data => setallMemeImg(data))
     },[])
 
+    // generation the images get random images
     function getimg(){
-    const data = allMemeImg.data.memes
-    const position = Math.floor(Math.random() * data.length)
+    // getting the data
+    const memesdata = allMemeImg.data.memes
+    // make a random number depends on the length of the data
+    const position = Math.floor(Math.random() * memesdata.length)
 
+    // getting the url from the data
+    const url = memesdata[position].url
+    
+    // changing the state of the memes state
     setMemes(previose => {
         return {
             ...previose, 
-            RandomImg : data[position].url}
+            RandomImg : url}
     })
 
     }
 
+    // make the input text display on the image
     function handelChange(event){
-        const {name , value } = event.target
+        const {name , value } = event.target //getting the inputs value
+
+        // change the stat of the top and bottom text
         setMemes(prevText =>{
         return {
         ...prevText,
@@ -44,12 +57,19 @@ function Main() {
     return (
         <main>
 
+            {/* button for generating the memes images */}
             {<button onClick = {getimg}>Get new meme image 🖼️</button>}
 
             <div className="inputsection">
+                {/* input for the top text */}
                 <input name='TopText' type="text" placeholder='text will display in  top of the image' value={memes.TopText} onChange={handelChange}/>
+
+                {/* input for the bottom text */}
                 <input name='BottomText' type="text" placeholder='text will display in  bottom of the image' value={memes.BottomText} onChange={handelChange}/>
+
             </div>
+            
+            {/* where the meme will display */}
             <div className="memeContainer">
                 <img src={memes.RandomImg} alt="" />
                 <h2 className='top'>{memes.TopText}</h2>
